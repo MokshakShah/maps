b29600f6fecad85e01417fee2d21673ca5bde9f4
MokshakShah <mokshak18@gmail.com>
2025-04-02 17:32:16 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'; // Import the `defineConfig` function from Vite
+import react from '@vitejs/plugin-react'; // Import the React plugin for Vite
 
 // https://vite.dev/config/
+// Export the Vite configuration using `defineConfig`
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react()], // Use the React plugin for Vite to handle React-related tasks
+});
